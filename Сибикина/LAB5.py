3b1c8a54e5b5aa840b05138b7e139af425f79bbc
RAdKuzur <drive16052003@gmail.com>
2023-12-08 21:15:23 +0400

    21:14 08/12/2023 by Rkuzur update LAB5.py

--- Сибикина/LAB5.py
+++ Сибикина/LAB5.py
@@ -44,54 +44,59 @@ def left_rotate_string(s, n):
 def right_rotate_string(s, n):
     n = n % len(s) 
     return s[-n:] + s[:-n]
+def amount_one(p):
+    n = 0
+    for i in range(len(p)):
+        if(p[i] == '1'):
+            n = n + 1
+    return n
 #начало программы
-a = input()
-k = len(a)
-d = k - 1
-r = math.ceil(math.log2(k+1+math.ceil(math.log2(k+1))))
-print(r)
-c = ""
-d = ""
-if (r == 3):
-    c = "1101"
-    d = "1000"
-a_10 = int(a,2)
-c_10 = int(c,2)
-d_10 = int(d,2)
-a_d = d_10*a_10 
-str_a_d = binar(a_d)
-while(len(str_a_d)<k+r):
-    str_a_d = '0' + str_a_d
-str_1 = divide_bin(str_a_d,c)
-str_10 = int(str_1,2)
-b = binar(str_10 ^ a_d)
-while(len(b)<k+r):
+a = input() #input combination
+k = len(a) #length code word
+r = math.ceil(math.log2(k+1+math.ceil(math.log2(k+1)))) #
+t = 1 #количество ошибок
+d = 2*t+1 #кодовое расстояние
+print("a = ", a , "\nk = ", k, "\nr = ", r, "\nd0 = ", d)
+
+
+P = ['11', '101', '111', '1001','1011','1101','1111','10001','10011','10101','10111',
+     '11101','11111','100001','100011','100101111','100111','101001','101011','101101','101111',
+     '110001','110101','110111']
+i = 0
+while i != len(P) and len(P[i])<=len(a):
+    p = P[i]  #проверочный многочлен
+    if(amount_one(p)>= d and len(p)>=r+1):   #сомнительный момент       
+        break
+    i = i + 1
+print("Образующий многочлен p = ",p)
+a_new = a
+for i in range(len(p)-1):
+    a_new = a_new + '0'
+print(a_new , ' % ', p , ' = ',  divide_bin(a_new, p))
+b = binar(int(a_new, 2) ^ int(divide_bin(a_new, p), 2))
+while len(b) < len(a_new):
     b = '0' + b
-print("a = ", a, "\n", "b = ", b, "\n", "c = ", c, "\n", )
+print(a_new , ' + ', divide_bin(a_new, p), ' = ', b)
+ost = divide_bin(a_new, p)
+print('b = ', b)
 
-### проверка ошибкиr
-print("хотите внести ошибку?\n")
-
+print('\n Хотите внести ошибку? ')
 b_error = input()
+print('b =       ', b)
+print('b_error = ', b_error)
 
-print("b_error = " , b_error)
-#print(left_rotate_string(b_error,1))
-ost = divide_bin(b_error, c)
+print( b_error , ' : ', p , ' = ', divide_bin(b_error, p))
+
+ost2 = divide_bin(b_error, p)
 amount_shift = 0
-while(int(ost,2) > 1):
+while int(ost2,2)>1:
     b_error = left_rotate_string(b_error,1)
-    ost = divide_bin(b_error, c)
     amount_shift = amount_shift + 1
+    ost2 = divide_bin(b_error, p)
+    print( b_error , ' : ', p , ' = ', ost2)
+str_10 = binar(int(b_error, 2) ^ int(ost2, 2))
+while len(str_10) < len(a_new):
+    str_10 = '0' + str_10
 
-
-b_error_int= int(b_error,2) ^ int(ost,2)
-b_error = binar(b_error_int)
-while(len(b_error)< k+r):
-    b_error = '0' + b_error
-b_error = right_rotate_string(b_error,amount_shift)
-if(amount_shift == 0):
-    print("ошибок нет")
-else:
-    print ("Исправленная комбинация: ", b_error ,"\n", "Ошибка допущена в ", amount_shift, "разряде" )
-
-
+b_error = right_rotate_string(str_10, amount_shift)
+print("b_true =  ", b_error)
