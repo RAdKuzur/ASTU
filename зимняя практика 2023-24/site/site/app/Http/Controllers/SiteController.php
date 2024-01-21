bc0bf417735adf9833f4a333dd4ae2fa8356d505
RAdKuzur <drive16052003@gmail.com>
2024-01-21 16:54:54 +0400

    16:55 21/01/2024 add laravel project

---
+++ зимняя практика 2023-24/site/site/app/Http/Controllers/SiteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Redirect;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+
+
+class SiteController extends Controller
+{
+    public function main(){
+        DB::table('test_models')->insert(['name' => 'Ivan', 'number' => 5]);
+        return view('main');
+
+    }
+
+
+    //
+}
---
+++ зимняя практика 2023-24/site/site/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\SiteController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/main',  [SiteController::class, 'main'])->name('main');
+
+Route::get('/', function () {
+    return view('welcome');
+});
