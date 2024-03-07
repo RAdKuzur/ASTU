b9ff2bf0ebf796ace4fc976983a4ff26950b3f56
RAdKuzur <drive16052003@gmail.com>
2024-03-07 19:47:53 +0400

    19:47 07/03/2024

---
+++ Технология программирования (6 семестр)/LAB_3_SADCHIKOV/LAB_3_SADCHIKOV/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LAB_3_SADCHIKOV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фраза";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "UNDER",
+            "NEAR",
+            "ON"});
+            this.checkedListBox1.Location = new System.Drawing.Point(327, 131);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(86, 49);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "FIRST",
+            "SECOND"});
+            this.checkedListBox2.Location = new System.Drawing.Point(216, 394);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(120, 34);
+            this.checkedListBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 368);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(410, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Location = new System.Drawing.Point(65, 131);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(133, 274);
+            this.checkedListBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(410, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "form";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "текст";
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Location = new System.Drawing.Point(620, 131);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(120, 274);
+            this.checkedListBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "English Phrase";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+    }
+}
+
---
+++ Технология программирования (6 семестр)/LAB_3_SADCHIKOV/LAB_3_SADCHIKOV/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_3_SADCHIKOV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox2.GetItemChecked(0))
+            {
+                checkedListBox3.Items.Add(textBox1.Text);
+            }
+            if (checkedListBox2.GetItemChecked(1))
+            {
+                checkedListBox4.Items.Add(textBox1.Text);
+            }
+            textBox1.Text = "";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+             label2.Text = "";
+             for (int i = 0; i < checkedListBox3.Items.Count; i++)
+             {
+                if (checkedListBox3.GetItemChecked(i))
+                {
+                    counter++;
+                }
+             }
+             int k = 0;
+             for(int i = 0; i < checkedListBox3.Items.Count; i++)
+             {
+                if (checkedListBox3.GetItemChecked(i))
+                {
+                    k++;
+                    if (i != 0)
+                    {
+                        if (k != counter)
+                            label2.Text = label2.Text + " , " + checkedListBox3.Items[i].ToString();
+                        else
+                        {
+                            label2.Text = label2.Text + " AND " + checkedListBox3.Items[i].ToString();
+                        }
+                    }
+                    else
+                    {
+                        label2.Text = checkedListBox3.Items[i].ToString();
+                    }
+                }
+             }
+
+
+
+            if (counter > 1)
+                {
+                label2.Text = label2.Text + " ARE ";
+                }
+             else
+             {
+                label2.Text = label2.Text + " IS ";
+            }
+            if (checkedListBox1.GetItemChecked(0))
+            {
+                label2.Text = label2.Text + " " +checkedListBox1.Items[0].ToString();
+            }
+            if (checkedListBox1.GetItemChecked(1))
+            {
+                label2.Text = label2.Text + " " + checkedListBox1.Items[1].ToString();
+            }
+            if (checkedListBox1.GetItemChecked(2))
+            {
+                label2.Text = label2.Text + " " + checkedListBox1.Items[2].ToString();
+            }
+
+            for (int i = 0; i < checkedListBox4.Items.Count; i++)
+            {
+                if (checkedListBox4.GetItemChecked(i))
+                {
+                    label2.Text = label2.Text + " " + checkedListBox4.Items[i].ToString();
+                }
+
+            }
+
+
+
+        }
+    }
+}
