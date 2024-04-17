4beba0ed799dde51192111135c3b58853309d37f
PR P. <Praktik@RST.EDU>
2024-04-17 15:06:19 +0400

    17/04/2024 15:06

--- Морозов 6 семестр/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Морозов 6 семестр/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2beb5f9e619027fb4beb457c876579204c16a096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ce3c852aa675022176c24207df60e8ba42fbd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
