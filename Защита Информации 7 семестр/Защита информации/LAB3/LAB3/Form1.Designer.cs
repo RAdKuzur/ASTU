535df6c02daae8be6bd42308b5ed1abc59bc473f
RAdKuzur <drive16052003@gmail.com>
2024-09-22 13:24:59 +0400

    22/09/2024 13:24 add LAB3

---
+++ Защита Информации 7 семестр/Защита информации/LAB3/LAB3/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace LAB3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            dataGridView2 = new DataGridView();
+            dataGridView3 = new DataGridView();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            button2 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            button4 = new Button();
+            richTextBox1 = new RichTextBox();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            listBox1 = new ListBox();
+            button3 = new Button();
+            label4 = new Label();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(10, 26);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(122, 258);
+            dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(205, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Ввод";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(151, 26);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(129, 258);
+            dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(301, 26);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(298, 258);
+            dataGridView3.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(629, 347);
+            tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(dataGridView3);
+            tabPage1.Controls.Add(dataGridView2);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(621, 319);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(500, 290);
+            button2.Name = "button2";
+            button2.Size = new Size(99, 23);
+            button2.TabIndex = 9;
+            button2.Text = "Ввод матрицы ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(301, 8);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 15);
+            label3.TabIndex = 8;
+            label3.Text = "PERMISSIONS";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(151, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 15);
+            label2.TabIndex = 7;
+            label2.Text = "OBJECTS";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 6;
+            label1.Text = "USERS";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button5);
+            tabPage2.Controls.Add(button4);
+            tabPage2.Controls.Add(richTextBox1);
+            tabPage2.Controls.Add(comboBox1);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(listBox1);
+            tabPage2.Controls.Add(button3);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(621, 319);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(359, 38);
+            button4.Name = "button4";
+            button4.Size = new Size(247, 28);
+            button4.TabIndex = 6;
+            button4.Text = "Редактировать";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(359, 72);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(246, 209);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(53, 6);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(178, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(13, 69);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 15);
+            label5.TabIndex = 3;
+            label5.Text = "OBJECTS";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(13, 87);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(218, 214);
+            listBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(359, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(246, 23);
+            button3.TabIndex = 1;
+            button3.Text = "Показать";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(13, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 15);
+            label4.TabIndex = 0;
+            label4.Text = "USER";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(531, 287);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 7;
+            button5.Text = "MOD";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(643, 365);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "LAB3";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private DataGridView dataGridView1;
+        private Button button1;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView3;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TabPage tabPage2;
+        private Button button2;
+        private Button button3;
+        private Label label4;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Label label5;
+        private RichTextBox richTextBox1;
+        private Button button4;
+        private Button button5;
+    }
+}
