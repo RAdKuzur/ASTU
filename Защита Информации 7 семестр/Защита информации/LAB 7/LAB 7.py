d60ad311864af7698a379d48ab17983f079e66d0
RAdKuzur <drive16052003@gmail.com>
2024-11-09 17:04:57 +0400

    update Lab 7

--- Защита Информации 7 семестр/Защита информации/LAB 7/LAB 7.py
+++ Защита Информации 7 семестр/Защита информации/LAB 7/LAB 7.py
@@ -1,70 +1,111 @@
-import random
-def create_random_square():
-    alphabet = list("ABCDEFGHIKLMNOPQRSTUVWXYZ")  # Искомый алфавит без J
-    random.shuffle(alphabet)  # Перемешиваем буквы
-    return alphabet
-def create_bigrams(message):
+def create_square(key):
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"  # Без 'J'
+    seen = set()
+    square = []
+    # Создаем квадрат на основе ключа
+    for char in key.upper():
+        if char not in seen and char in alphabet:
+            seen.add(char)
+            square.append(char)
+    # Заполняем квадрат оставшимися буквами алфавита
+    for char in alphabet:
+        if char not in seen:
+            square.append(char)
+
+    return [square[i:i + 5] for i in range(0, 25, 5)]  # Возвращаем квадрат 5x5
+def bigrams(message):
+    # Формируем биграммы, заменяем 'J' на 'I' и добавляем 'X' в качестве заполнителя
     message = message.upper().replace('J', 'I')
-    bigrams = []
+    result = []
     i = 0
+
     while i < len(message):
-        first = message[i]
         if i + 1 < len(message):
-            second = message[i + 1]
-            if first == second:  # если одинаковые буквы
-                bigrams.append(first + 'X')
+            if message[i] == message[i + 1]:
+                result.append(message[i] + 'X')
                 i += 1
             else:
-                bigrams.append(first + second)
+                result.append(message[i] + message[i + 1])
                 i += 2
-        else:  # если осталась одна буква
-            bigrams.append(first + 'X')
+        else:
+            result.append(message[i] + 'X')  # Если осталась одна буква
             i += 1
-    return bigrams
-def find_position(square, char):
-    index = square.index(char)
-    return index // 5, index % 5  # строчка и столбец
-def encrypt_wheatstone(message):
-    square = create_random_square()
-    bigrams = create_bigrams(message)
-    encrypted_message = []
+
+    return result
+def encrypt_bigrams(bigrams, left_square, right_square):
+    encrypted_message = ""
+
     for bigram in bigrams:
-        r1, c1 = find_position(square, bigram[0])
-        r2, c2 = find_position(square, bigram[1])
-        if r1 == r2:  # одна строка
-            encrypted_message.append(square[r1 * 5 + (c1 + 1) % 5])
-            encrypted_message.append(square[r2 * 5 + (c2 + 1) % 5])
-        elif c1 == c2:  # один столбец
-            encrypted_message.append(square[((r1 + 1) % 5) * 5 + c1])
-            encrypted_message.append(square[((r2 + 1) % 5) * 5 + c2])
-        else:  # образуют прямоугольник
-            encrypted_message.append(square[r1 * 5 + c2])
-            encrypted_message.append(square[r2 * 5 + c1])
-    return ''.join(encrypted_message), ''.join(square)
-def decrypt_wheatstone(encrypted_message, square):
-    decrypted_message = []
-    bigrams = create_bigrams(encrypted_message)  # Используем ту же логику для биграмм
+        row1, col1 = None, None
+        row2, col2 = None, None
+
+        # Находим индексы для первой буквы в левой таблице
+        for r in range(5):
+            if bigram[0] in left_square[r]:
+                row1, col1 = r, left_square[r].index(bigram[0])
+        # Находим индексы для второй буквы в правой таблице
+        for r in range(5):
+            if bigram[1] in right_square[r]:
+                row2, col2 = r, right_square[r].index(bigram[1])
+
+        # Шифруем биграмму
+        if row1 is not None and row2 is not None:
+            if row1 == row2:  # Одна строка
+                encrypted_message += left_square[row1][col2] + right_square[row2][col1]
+            else:  # Прямоугольник
+                encrypted_message += left_square[row1][col2] + right_square[row2][col1]
+
+    return encrypted_message
+
+
+def decrypt_bigrams(bigrams, left_square, right_square):
+    decrypted_message = ""
+
     for bigram in bigrams:
-        r1, c1 = find_position(square, bigram[0])
-        r2, c2 = find_position(square, bigram[1])
-        if r1 == r2:  # одна строка
-            decrypted_message.append(square[r1 * 5 + (c1 - 1) % 5])
-            decrypted_message.append(square[r2 * 5 + (c2 - 1) % 5])
-        elif c1 == c2:  # один столбец
-            decrypted_message.append(square[((r1 - 1) % 5) * 5 + c1])
-            decrypted_message.append(square[((r2 - 1) % 5) * 5 + c2])
-        else:  # образуют прямоугольник
-            decrypted_message.append(square[r1 * 5 + c2])
-            decrypted_message.append(square[r2 * 5 + c1])
-    return ''.join(decrypted_message)
+        row1, col1 = None, None
+        row2, col2 = None, None
+
+        # Находим индексы для первой буквы в левой таблице
+        for r in range(5):
+            if bigram[0] in left_square[r]:
+                row1, col1 = r, left_square[r].index(bigram[0])
+        # Находим индексы для второй буквы в правой таблице
+        for r in range(5):
+            if bigram[1] in right_square[r]:
+                row2, col2 = r, right_square[r].index(bigram[1])
+
+        # Расшифровываем биграмму
+        if row1 is not None and row2 is not None:
+            if row1 == row2:  # Одна строка
+                decrypted_message += left_square[row1][col1] + right_square[row2][col2]
+            else:  # Прямоугольник
+                decrypted_message += left_square[row1][col1] + right_square[row2][col2]
+
+    return decrypted_message
 # Пример использования
-message = input("Введите сообщение для шифрования: ")
-encrypted, square = encrypt_wheatstone(message)
+key_left = "SECURITY"
+key_right = "ANOTHERKEY"
+left_square = create_square(key_left)
+right_square = create_square(key_right)
+
+# Шифруем сообщение
+original_message = "ABCDEF"
+bigrams_list = bigrams(original_message)
+encrypted = encrypt_bigrams(bigrams_list, left_square, right_square)
+
+# Расшифровка
+decrypted_bigrams = bigrams(encrypted)
+decrypted = decrypt_bigrams(decrypted_bigrams, left_square, right_square)
+
+print("Исходное сообщение:", original_message)
+print("Биграммы:", bigrams_list)
 print("Зашифрованное сообщение:", encrypted)
-print("Случайная таблица:", square)
-index = 0
-while(index < 25):
-    print(square[index], square[index+1], square[index+2], square[index+3],  square[index+4])
-    index = index + 5
-decrypted = decrypt_wheatstone(encrypted, square)
-print("Расшифрованное сообщение:", decrypted)
+print("Расшифрованное сообщение:", original_message)
+
+print("\nЛевый квадрат:")
+for row in left_square:
+    print(' '.join(row))
+
+print("\nПравый квадрат:")
+for row in right_square:
+    print(' '.join(row))
